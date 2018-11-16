08e8b7dacc702f676a8078af5e0abd5d399ab8a9
Emanuel Nilsson <emannils1@lycksele.se>
2018-11-16 11:18:05 +0100

    Arrays and stuff

--- projects/Javascript 101/assets/js/5-array.js
+++ projects/Javascript 101/assets/js/5-array.js
@@ -3,4 +3,46 @@ var friend2 = "Inge";
 var friend3 = "Ing";
 var friend4 = "In";
 
-var friends = ["Ingen", "Inge", "Ing", "in"];
+var friends = ["Ingen", "Inge", "Ing", "In"];
+// startar på 0
+console.log(friends[0] + " " + friend1);
+
+var colours = ["Red", "Green", "Blue"];
+var colors = new Array();
+
+//push() - Lägga till en grej till en array
+
+colours.push("Magneta");
+console.log(colours);
+
+//pop() - Tar bort den sista grejen i en array
+colours.pop();
+console.log(colours);
+
+//unshift() - Lägger till en grej till början av en array
+colours.unshift("Despacito");
+console.log(colors);
+
+//shift() - Tar bort en grej till början av en array
+colours.shift();
+console.log(colours);
+
+//indexOf() - Hittar indexen av en grej i en array
+console.log(colours.indexOf("Green"));
+console.log(colours.indexOf("Cyan"));
+
+//slice() - Kopiera delar av en array
+var fruits = ["Mango", "Banan", "Citron", "Ananas", "Apelsin", "lol"]
+
+var citrus = fruits.slice(2, 4);
+console.log(citrus);
+
+var copyFruits = fruits.slice(fruits);
+console.log(copyFruits);
+
+//            0         1       2        3         4          5           6          7
+var cars = ["Audi", "Volvo", "Ford", "Ferrari", "Lada", "Lamborghini", "Subaru", "Citroën"];
+for(var i = 0; i < cars.length; i++) {
+    console.log(cars[i]);
+}
+
