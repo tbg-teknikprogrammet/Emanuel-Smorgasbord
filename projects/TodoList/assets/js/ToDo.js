7d17c933b1f103565aea99ecf665abcd995b0b46
Emanuel Nilsson <emannils1@lycksele.se>
2018-12-06 14:03:30 +0100

    Did nothing of value, life is pain

--- projects/TodoList/assets/js/ToDo.js
+++ projects/TodoList/assets/js/ToDo.js
@@ -1,18 +1,34 @@
 
 document.getElementsByTagName("button")[0].addEventListener("click", List)
 
- ToDoList = []
+var ToDoList = []
 
-function List(ToDoList) {
+function List() {
 
-    if(prompt("") == 'n') {
-        List.push("lol");
-        console.log(ToDoList)
-    }
-    elseif()
-        
+    var putin = 0;
+     
+        while(putin != "q" || putin != "Q") {
 
+            putin = prompt("Vad vill du göra?")
+
+            if(putin == "L" || putin == "l") {
+                console.log(ToDoList) // Visar listan
+                // prompt(ToDoLista.length)
+             }  
+             
+             else if(putin == "N" || putin == "n") {
+                 ToDoList.push(" "); //Lägger till, till listan
+                 console.log(ToDoList)
+             }
+             
+             else if(putin == "R" || putin == "r") {
+                 ToDoList.slice() // Tar bort från listan
+                 console.log(ToDoList)
+                 prompt(ToDoList)
+             }
     
+            //  else if(putin != "q" || putin != "Q")
+            //     break;
+        }
+      
 }
-
- 
