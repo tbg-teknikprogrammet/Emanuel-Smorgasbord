23cb5c90315e3465d34f94d6f7558e146d07f331
Emanuel Nilsson <emannils1@lycksele.se>
2018-12-13 14:11:29 +0100

    Created 6-objects.js

---
+++ projects/Javascript 101/assets/js/6-objects.js
@@ -0,0 +1,91 @@
+var person = ["Sid", 25, "Land of ooo"];
+
+console.log(person[2]);
+
+// Reverse the order of data
+
+var person2 = [32, "Riverwood", "Bert"]; // BorLänge
+
+console.log(person2[2]);
+
+// To model a person is perfect use case for object
+
+var person = {
+    name: "Bob", // Key: value pair
+    age: 46, 
+    hometown: "California"
+};
+
+// Order is non-existent in objects
+
+// Take data from objects
+console.log("This person's name is: " + person.name);
+
+console.log(person.age);
+
+person.age += 1;
+
+console.log(person.age);
+
+person.hometown = "pölse";
+
+
+var oddNums = {}; // Empty object and add more items later on
+oddNums.numberlolz = 1;
+oddNums.String = "one";
+oddNums.isOdd = true; // Made up by myself
+
+// All at once
+
+var doge = {
+    name: "Doggo",
+    breed: "Border Collie",
+    age: 3,
+    isFriend: true
+}
+
+var fish = new Object();
+fish.name = "Fish";
+fish.type = "Fish";
+fish.age = 3;
+fish.isLazy = false;
+
+
+var junkObject = {
+    name: "Junkrat",
+    age: 41,
+    color: "Explotions",
+    isEvil: true,
+    friends: ["Roadhog", "none1", "none3"],
+    pet: {
+        name: "Explotions",
+        species: "Bomb",
+        age: 20
+    }
+}
+
+console.log(junkObject.pet.species)
+console.log(junkObject.friends[0])
+console.log(junkObject.isEvil)
+
+var books = [
+    { // Index 0
+        author: "Emanuel Nilsson", 
+        title: "How2Dab", 
+        comments: ["So good! 10/10", "Such trash"]
+    }, 
+    { // Index 1
+        author: "Ben Shapiro", 
+        title: "Rek libtards 101", 
+        comments: ["This book is epic!", "How could i have lived without this book!"]
+    }
+];
+
+// array[index].objectProperty[index]
+// console.log(books[1].comments[0]) // "This book is epic!"
+
+for(var i = 0; i < books.length; i++) {
+    console.log(books[i].title, books[i].author, books[i].comments);
+}
+
+
