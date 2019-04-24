4a9b639083e26570d0b71dd12d851a973eb16907
Emanuel Nilsson <emannils1@lycksele.se>
2019-04-24 17:12:59 +0200

    Did some more stuff

--- projects/JS-Playground/assets/js/JS-Playground.js
+++ projects/JS-Playground/assets/js/JS-Playground.js
@@ -12,34 +12,58 @@ OnBigImageClick.addEventListener("click", function() {
     BigImageClass.style.visibility = "hidden";
 });
 
-
+// Sliderino
 var SlideNum = 1;
-var ButtonRight = document.getElementsByClassName("slide-right");
-var ButtonLeft = document.getElementsByClassName("slide-left");
 var ImageOne = "assets/images/mountains.jpg";
 var ImageTwo = "assets/images/mountains2.png";
 var ImageThree = "assets/images/mountains3.png";
 
-ButtonRight.addEventListener("click", function() {
-    SlideNum++
+var ButtonRight = document.querySelector(".slide-right"); // Select
+ButtonRight.addEventListener("click", function() { // Manipulate
+    SlideNum++;
 });
 
-ButtonLight.addEventListener("click", function() {
-    SlideNum--
+var ButtonLeft = document.querySelector(".slide-left"); // Select
+ButtonLeft.addEventListener("click", function() { // Manipulate
+    SlideNum--;
 });
-
 
 if(SlideNum = 1) {
-    ImageTwo.style.visibility = "hidden";
-    ImageThree.style.visibility = "hidden";
+    ImageTwo.visibility = "hidden";
+    ImageThree.visibility = "hidden";
 } 
 
 else if(SlideNum = 2) {
     ImageOne.style.visibility = "hidden";
-    ImageThree.style.visibility = "hidden";
+    ImageThree.visibility = "hidden";
 }
 
-else if(SlideNum = 3){
-    ImageOne.style.visibility = "hidden";
-    ImageTwo.style.visibility = "hidden";
+else if(SlideNum = 3) {
+    ImageOne.visibility = "hidden";
+    ImageTwo.visibility = "hidden";
 }
+
+// Color changing button
+
+document.addEventListener("keydown", function(event) { // Listens to button press
+    if(event.which == 67) { // If the button is c, do something
+        document.querySelector(".sec3").style.backgroundColor = "#eb1f1f";
+    }
+    else if(event.which == 66) {
+        document.querySelector(".sec3").style.backgroundColor = "#3ad31c"; // Green
+    }
+
+    else if(event.which == 65) {
+        document.addEventListener("keydown", function() {
+            setInterval(function() {
+                document.getElementsByTagName("body")[0].classList.toggle("toggleBg")
+            }, 200);
+        });
+    }
+
+    else {
+        document.querySelector(".sec3").style.backgroundColor = "#302e2e";
+    }
+  
+    
+});
