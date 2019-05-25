c8062938eadfb1873920064d0757c5ff32c71bae
Emanuel Nilsson <emannils1@lycksele.se>
2019-05-25 14:15:34 +0200

    Copied in scorekeeper and started on video player

--- projects/JS-Playground/assets/js/JS-Playground.js
+++ projects/JS-Playground/assets/js/JS-Playground.js
@@ -67,27 +67,22 @@ document.addEventListener("keydown", function(event) { // Listens to a button pr
     else if(event.which == 66) { // B
         document.querySelector(".sec3").style.backgroundColor = "#3ad31c";
     }
-    
     else if(event.which == 65) { // Attempt for "A" button that changes colors automatically
-        do {
-            var wow = document.addEventListener("keydown", function(event) {
-                setInterval(function() {
-                    document.getElementsByTagName("body")[0].classList.toggle("toggleBg") // toggleBg is the class that adds the color
-                }, 1000); 
-            });
-            if(wow.which == 27)
-                clearInterval();
-        } while(wow.which != 27);
+        var j = setInterval(function() {
+            document.getElementsByTagName("body")[0].classList.toggle("toggleBg") // toggleBg is the class that adds the color
+        }, 1000);  
+        if(event.which == 27) { // This never runs.
+            alert("You pressed: esc");
+            clearInterval(j); // I gave up...
+        }      
     }
-
     else {
         document.querySelector(".sec3").style.backgroundColor = "#302e2e"; // Any other button is presed, return to normal
-    }
-  
+        clearInterval(j);
+    }  
 });
 
 // Clicker game?
-
 var Clicks = 0;
 var ButtonClicked = false;
 var UpgradeButton = document.querySelector(".UpgradeButton");
@@ -123,3 +118,76 @@ if(Clicks >= 10 && Clicks <= 11) { // Do something when Clicks are at 10
 UpgradeButton2.addEventListener("click", function() {
     ClickerWin.style.visibility = "visible";
 });
+
+// Scorekeeper ------------------------------------------------------------------
+var win = false;
+var score = 0; // Playing to
+var Player1 = 0;
+var Player2 = 0;
+var Player1Class = document.querySelector("#Sc-one");
+var Player2Class = document.querySelector("#Sc-two");
+
+// How long will you play to?
+var PlayingTo = document.querySelector(".userInp"); // Select
+PlayingTo.addEventListener("click", function() {
+    document.querySelector("#PlayingTo").innerHTML = PlayingTo.value;
+});
+//**********************
+PlayingTo.value = 1;
+
+// Select the Player1 -- ONE
+var BtnPlayer1 = document.querySelector("#P1"); // Select
+BtnPlayer1.addEventListener("click", function() { // Manipulate
+    if(win == false) {
+        Player1 += 1; // Adds numbers to html
+        document.querySelector("#Sc-one").innerHTML = Player1; // A variable because it needs to change  
+    } 
+
+    if(Player1 == PlayingTo.value) { 
+        win = true;
+        document.querySelector("#Wining-text").innerHTML = "<strong> Player one won! </strong>";
+        Player1Class.classList.add("ColorWin");
+        Player2Class.classList.add("ColorLose");
+    }
+});
+//***********************
+
+
+// Select the Player2 -- TWO
+var BtnPlayer2 = document.querySelector("#P2"); // Select
+BtnPlayer2.addEventListener("click", function() { // Manipulate
+    if(win == false) {
+        Player2 += 1;
+        document.querySelector("#Sc-two").innerHTML = Player2;
+    }
+
+    if(Player2 == PlayingTo.value) {
+        win = true;
+        document.querySelector("#Wining-text").innerHTML = "<strong> Player two won! </strong>";
+        Player2Class.classList.add("ColorWin");
+        Player1Class.classList.add("ColorLose");
+    }
+        
+});
+//***********************
+
+// Reset the scorekeeper
+var BtnReset = document.querySelector(".RS"); // Select
+BtnReset.addEventListener("click", function() { // Manipulate
+    win = false;
+    score = 0;
+    Player1 = 0;
+    document.querySelector("#Sc-one").innerHTML = Player1;
+    Player2 = 0;
+    document.querySelector("#Sc-two").innerHTML = Player2;
+
+    document.querySelector("#Wining-text").innerHTML = "";
+    document.querySelector("#PlayingTo").innerHTML = "0";
+    Player2Class.classList.remove("ColorWin");
+    Player1Class.classList.remove("ColorWin");
+    Player1Class.classList.remove("ColorLose");
+    Player2Class.classList.remove("ColorLose");
+
+});
+//**********************
+//------------------------------------------------------------------------------------
